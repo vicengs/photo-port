cea5191b20bc72792fa0a9d1c0f0d85e99acb5fd
Vicente Garcia <vicen1@hotmail.com>
2022-06-14 12:38:31 -0500

    Add tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+afterEach(cleanup);
+describe('About component', () => {
+    it('renders', () => {
+        render(<About />);
+    });
+    it('Matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 function Nav() {
   const categories = [
     { name: "commercial"
@@ -11,29 +12,30 @@ function Nav() {
      ,description: "Fields, farmhouses, waterfalls, and the beauty of nature" }
   ];
   function categorySelected(name) {
-    console.log(`${name} clicked`)
+    console.log(`${name} clicked`);
+    return name;
   }
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
-        <a href="/">
+      <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">
+          <a data-testid="about" href="#about" onClick={() => categorySelected("About")}>
               About me
             </a>
           </li>
-          <li>
-            <span>Contact</span>
+          <li className={"mx-2"}>
+            <span onClick={() => categorySelected("Contact")}>Contact</span>
           </li>
           {categories.map((category) => (
             <li className="mx-1" key={category.name} >
-              <span onClick={() => categorySelected(category.name)}>
-                {category.name}
+              <span onClick={() => { categorySelected(category.name); }}>
+                {capitalizeFirstLetter(category.name)}
               </span>
             </li>
           ))}
