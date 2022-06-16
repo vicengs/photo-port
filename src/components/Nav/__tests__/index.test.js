d75f3fd8a44f03eee9ddaf3ab592669da7161c76
Vicente Garcia <vicen1@hotmail.com>
2022-06-16 11:17:41 -0500

    Add contact component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -7,6 +7,8 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 afterEach(cleanup);
 describe('Nav component', () => {
     it('renders', () => {
@@ -14,6 +16,8 @@ describe('Nav component', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
           />);
     });
     it('matches snapshot', () => {
